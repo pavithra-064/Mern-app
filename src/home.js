30bbbc477fe840d35340770e415f0d74e1c100a4
Pavithra Subramanian <pavithrasubramanian21@gmail.com>
2021-12-14 21:56:28 +0530

    Update build

--- src/home.js
+++ src/home.js
@@ -10,7 +10,7 @@ function App2(){
  // const[loginStatus,setLoginStatus]=useState('');
   
   const login =() =>{
-   Axios.post("https://mern-crud-url.herokuapp.com/login", {
+   Axios.post("https://url-shortner-mern.herokuapp.com/login", {
      username: username,
      password: pd,
   }).then(res =>{
@@ -27,14 +27,16 @@ function App2(){
       <div className="homie">
     <div className="Home"><h1>WELCOME!</h1>
   <h2>Login</h2>
+  <form>
   <div className="form">
   <label className="labell">USERNAME:</label>
   <input  type="text" name="username" onChange={(e)=>{setUsername(e.target.value);}}/>
   <label className="labell">PASSWORD:</label>
-  <input type="text" name="password"  onChange={(e)=>{setPd(e.target.value);}}/>
+  <input type="password"  onChange={(e)=>{setPd(e.target.value);}}/>
   <button variant="btn btn-success" target="_blank" onClick={login}>LOGIN</button>
   <h4 className="labell">NEW USER :<Button target="_blank" onClick={() => history.push('/register')}>Register</Button> Here </h4>
   </div>
+  </form>
   </div>
   </div>
 );
--- src/register.js
+++ src/register.js
@@ -14,7 +14,7 @@ function App(){
 
    const submitreg = () =>{
      alert("successful!");
-    Axios.post("https://mern-crud-url.herokuapp.com/register", {
+    Axios.post("https://url-shortner-mern.herokuapp.com/register", {
       username: username,
       password: password, 
       useremail:useremail
