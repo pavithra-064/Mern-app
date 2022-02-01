5ef5a43ff070151b68bc3b5ce2c9b42f88e5a714
pavithra-064 <pavithrasubramanian21@gmail.com>
2022-02-01 22:46:13 +0530

    Update server

--- index.js
+++ index.js
@@ -130,13 +130,13 @@ res.redirect(shortUrl.longUrl)
 const PORT = process.env.PORT || 3001;
 
  
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
   });
   
-}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
