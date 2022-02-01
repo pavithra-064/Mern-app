94fc722f64c150df7b5bc121bdfd5021c7f4b513
pavithra-064 <pavithrasubramanian21@gmail.com>
2022-02-01 22:27:01 +0530

    Update Client

--- index.js
+++ index.js
@@ -133,6 +133,9 @@ const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
