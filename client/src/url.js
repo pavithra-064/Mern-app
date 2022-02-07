62f71f04c22f016d37c44652bf3d5e6a2611f881
pavithra-064 <pavithrasubramanian21@gmail.com>
2022-02-07 11:02:06 +0530

    Update api links

--- client/src/url.js
+++ client/src/url.js
@@ -1,48 +1,46 @@
- 
 import "./App.css";
 import { useEffect, useRef, useState } from "react";
-import Axios from 'axios';
-import history from './history';
- 
+import Axios from "axios";
+import history from "./history";
+
 function App3() {
-    const [longUrl, setLongUrl] = useState("");
-    const [errMess, setErrMess] = useState("");
-    const [shortenedUrl, setShortenedUrl] = useState("");
-    const shortenedinput = useRef(null);
-   //const[shortUrl,setShortUrll]=useState("");
-    const shortid = require("shortid");
-    /*const baseUrl="https://url-shortner-mern.herokuapp.com";*/
-    const baseUrl = "http://127.0.0.1:5000";
-   
-    const [shortUrl,setShortUrl] = useState("");
-    const [shortCode,setShortCode] = useState("");
-    const submitre = () =>{
-       Axios.post("http://localhost:5000/api/url", {
-         longUrl:longUrl,
-         shortenedUrl:shortCode,
-         shortUrl:shortUrl
-     })
+  const [longUrl, setLongUrl] = useState("");
+  const [errMess, setErrMess] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+  const shortenedinput = useRef(null);
+  //const[shortUrl,setShortUrll]=useState("");
+  const shortid = require("shortid");
+  /*const baseUrl="https://url-shortner-mern.herokuapp.com";*/
+  const baseUrl = "http://127.0.0.1:5000";
+
+  const [shortUrl, setShortUrl] = useState("");
+  const [shortCode, setShortCode] = useState("");
+  const submitre = () => {
+    Axios.post("/api/url", {
+      longUrl: longUrl,
+      shortenedUrl: shortCode,
+      shortUrl: shortUrl,
+    });
+  };
+
+  useEffect(() => {
+    if (errMess) {
+      const timeout = setTimeout(() => {
+        setErrMess("");
+      }, 10000);
+      return () => {
+        clearTimeout(timeout);
       };
-   
- 
-    useEffect(() => {
-        if (errMess) {
-            const timeout = setTimeout(() => {
-                setErrMess("");
-            }, 10000);
-            return () => {
-                clearTimeout(timeout);
-            };
-        }
-    }, [errMess]);
- 
-    function handleCopy() {
-        if (shortenedUrl) {
-            navigator.clipboard.writeText(shortenedUrl);
-        }
-        submitre();
     }
-/*
+  }, [errMess]);
+
+  function handleCopy() {
+    if (shortenedUrl) {
+      navigator.clipboard.writeText(shortenedUrl);
+    }
+    submitre();
+  }
+  /*
     const check =() =>{
         Axios.post("http://localhost:5000/check", {
           longUrl:longUrl,
@@ -56,78 +54,79 @@ function App3() {
        })
     }
 */
-       
- 
-     const handleSubmit=(e)=> {
-        Axios.post("http://localhost:5000/check", {
-            longUrl:longUrl,
-         }).then(res =>{
-          if(res.data.message==="null")
-          {
-            const urlCode = shortid.generate();
-    console.log(urlCode);
-        const shortcode=baseUrl + "/" + urlCode;
+
+  const handleSubmit = (e) => {
+    Axios.post("/api/check", {
+      longUrl: longUrl,
+    }).then((res) => {
+      if (res.data.message === "null") {
+        const urlCode = shortid.generate();
+        console.log(urlCode);
+        const shortcode = baseUrl + "/" + urlCode;
         setShortCode(urlCode);
         setShortUrl(shortcode);
         setShortenedUrl(shortcode);
         e.preventDefault();
-          }
-          else
-          {
-           
-            setShortUrl(res.data.message);
-            e.preventDefault();
-            }      
-    })
-}
- 
-    return (
-        <main className="mainn">
-            <button className="btn" onClick={() => history.push('/')}>Logout</button>
-        <header className="header">
-            <h2>Create Short URL</h2>
-        </header>
-        <section className="App">
-            <div className="form">
-            <h2 className="labell">Paste your long URL here:</h2>
-            <p className="error-message">{errMess}</p>
-            <div className="input">
-                <form>
-                    <div className="form-control">
-                        <input
-                            type="text"
-                            placeholder="long url"
-                            name="fullUrl"
-                            value={longUrl}
-                            onChange={(e) => setLongUrl(e.target.value)}
-                        />
-                        <button type="button" id="create-short-url"  onClick={handleSubmit}>Shorten</button>
-                       
-                    </div>
-                </form>
+      } else {
+        setShortUrl(res.data.message);
+        e.preventDefault();
+      }
+    });
+  };
+
+  return (
+    <main className="mainn">
+      <button className="btn" onClick={() => history.push("/")}>
+        Logout
+      </button>
+      <header className="header">
+        <h2>Create Short URL</h2>
+      </header>
+      <section className="App">
+        <div className="form">
+          <h2 className="labell">Paste your long URL here:</h2>
+          <p className="error-message">{errMess}</p>
+          <div className="input">
+            <form>
+              <div className="form-control">
+                <input
+                  type="text"
+                  placeholder="long url"
+                  name="fullUrl"
+                  value={longUrl}
+                  onChange={(e) => setLongUrl(e.target.value)}
+                />
+                <button
+                  type="button"
+                  id="create-short-url"
+                  onClick={handleSubmit}
+                >
+                  Shorten
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="output">
+            <p className="labell">Short URL:</p>
+            <div className="form-control">
+              <input
+                type="text"
+                name="short-url"
+                value={shortUrl}
+                ref={shortenedinput}
+                onChange={(e) => {
+                  setShortUrl(e.target.value);
+                }}
+              />
+              <button type="button" onClick={handleCopy}>
+                Copy
+              </button>
             </div>
-            <div className="output">
-                <p  className="labell">Short URL:</p>
-                <div className="form-control">
-                    <input
-                        type="text"
-                        name="short-url"
-                       
-                        value={shortUrl}
-                        ref={shortenedinput}
-                        onChange={(e)=>{setShortUrl(e.target.value)}}
-                    />
-                    <button type="button" onClick={handleCopy}>
-                        Copy
-                    </button>
-                </div>
-            </div>
-            </div>
-   
-        </section>
+          </div>
+        </div>
+      </section>
     </main>
-               
-    );
+  );
 }
- 
+
 export default App3;
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 //const bodyParser = require("body-parser")
-const cors = require("cors")
-require("dotenv").config()
-const app = express()
-const path=require("path");
- 
-const model=require("./models/User");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const path = require("path");
+
+const model = require("./models/User");
 const link = require("./models/Link");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 /*
 const connectDB = async () => {
   try {
@@ -28,115 +29,115 @@ const connectDB = async () => {
 /*mongoose.connect("mongodb+srv://pavithra-064:Shaposu3217@cruddb.si1xg.mongodb.net/cruddb?retryWrites=true&w=majority&ssl=true", 
 mongoose.connect("mongodb+srv://pavithra-064:Shaposu3217@cruddb-shard-00-00.si1xg.mongodb.net:27017,cruddb-shard-00-01.si1xg.mongodb.net:27017,cruddb-shard-00-02.si1xg.mongodb.net:27017/cruddb?ssl=true&replicaSet=atlas-c56vv8-shard-0&authSource=admin&retryWrites=true&w=majority",
 */
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://pavithra-064:Shaposu3217@cruddb.si1xg.mongodb.net/cruddb?retryWrites=true&w=majority",{
- }, 
-()=>{
-   console.log("DB connected");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://pavithra-064:Shaposu3217@cruddb.si1xg.mongodb.net/cruddb?retryWrites=true&w=majority",
+  {},
+  () => {
+    console.log("DB connected");
+  }
+);
+
+app.post("/api/register", async (req, res) => {
+  const username = req.body.username;
+  const useremail = req.body.useremail;
+  const password = req.body.password;
+  const user = new model({
+    username: username,
+    useremail: useremail,
+    password: password,
+  });
+
+  try {
+    await user.save();
+    res.send("inserted data");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post('/register',async(req,res)=>{
-    const username =req.body.username;
-    const useremail=req.body.useremail;
-    const password=req.body.password;
-    const user= new model({username:username, useremail:useremail,password:password});
-   
-    try{
-      await user.save();
-      res.send("inserted data");
-    }catch(err){
-      console.log(err);
-    }
-});
- 
-app.post('/login',async(req,res)=>{
-  const username=req.body.username;
- 
-  const password=req.body.password ;
+app.post("/api/login", async (req, res) => {
+  const username = req.body.username;
 
-   await model.findOne({ username:username}, (err,user)=>{
-    if(err){
-      console.log(err);
-      res.send({message: err});
-    }
-    if(user){
-  
-      if(password===user.password){
-        
-        res.send({message:"Login Successful",user:user})
-      }else{
-        console.log("user exist");
-        res.send({message:" Password didn't match"})
+  const password = req.body.password;
+
+  await model
+    .findOne({ username: username }, (err, user) => {
+      if (err) {
+        console.log(err);
+        res.send({ message: err });
       }
-    } else{
-      res.send({message:"User not registered"})
-    }
-  }).clone();
- 
-})
+      if (user) {
+        if (password === user.password) {
+          res.send({ message: "Login Successful", user: user });
+        } else {
+          console.log("user exist");
+          res.send({ message: " Password didn't match" });
+        }
+      } else {
+        res.send({ message: "User not registered" });
+      }
+    })
+    .clone();
+});
 
+app.post("/api/url", async (req, res) => {
+  const longUrl = req.body.longUrl;
+  const shortenedUrl = req.body.shortenedUrl;
+  const shortUrl = req.body.shortUrl;
+  const Link = new link({
+    longUrl: longUrl,
+    shortenedUrl: shortenedUrl,
+    shortUrl: shortUrl,
+  });
+  try {
+    await Link.save();
+    res.send("Inserted data");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.post("/api/url",async(req,res)=>{
-  const longUrl=req.body.longUrl;
-  const shortenedUrl=req.body.shortenedUrl;
-  const shortUrl=req.body.shortUrl;
-  const Link = new link({longUrl:longUrl,shortenedUrl:shortenedUrl,shortUrl:shortUrl});
- try{
-  await Link.save();
-  res.send("Inserted data");
-}catch(err){
-  console.log(err);
-}
-});  
- 
-app.post("/check",async(req,res)=>{
-  const longUrl=req.body.longUrl;
-  link.findOne({longUrl:longUrl},(err,url)=>{
-    if(err){
-      res.send({err:err});
+app.post("/api/check", async (req, res) => {
+  const longUrl = req.body.longUrl;
+  link.findOne({ longUrl: longUrl }, (err, url) => {
+    if (err) {
+      res.send({ err: err });
     }
-    if(url){
-      res.send({message:url.shortUrl})
-   
+    if (url) {
+      res.send({ message: url.shortUrl });
+    } else {
+      res.send({ message: "null" });
     }
-    else{
-      res.send({message:"null"})
-   
-    }
-  })
-})
- 
-app.get("/read",async(req,res)=>{
-  model.find({},(err,result)=>{
-    if(err){
+  });
+});
+
+app.get("/api/read", async (req, res) => {
+  model.find({}, (err, result) => {
+    if (err) {
       res.send(err);
-    }else {
+    } else {
       res.send(result);
     }
   });
 });
- 
+
 /*app.post('/shortUrl',async (req,res)=>{
   await link.create({ longUrl:req.body.fullUrl})
   res.redirect('/')
 })*/
- 
-app.get("/*", async(req,res)=>{
-  const shortenedUrl=req.params[0]
-const shortUrl= await link.findOne({ shortenedUrl:shortenedUrl})
-if(!shortUrl) return res.sendStatus(404)
-res.redirect(shortUrl.longUrl)
-})
- 
-const PORT = process.env.PORT || 3001;
 
- 
+app.get("/api/*", async (req, res) => {
+  const shortenedUrl = req.params[0];
+  const shortUrl = await link.findOne({ shortenedUrl: shortenedUrl });
+  if (!shortUrl) return res.sendStatus(404);
+  res.redirect(shortUrl.longUrl);
+});
 
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
-  app.get("*", (req, res) => {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
-  
-
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
